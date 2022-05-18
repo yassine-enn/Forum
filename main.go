ed850e4f8dedd8888f60a4f38bf760d5bda6b82d
Yassine Ennouhi <root@LAPTOP-8BN54FDS.localdomain>
2022-05-18 09:13:19 +0200

    first tests, not working

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./forum2.db")
+	if err != nil {
+		fmt.Println("Echec de l'ouverture de la base")
+		return
+	}
+	result, err1 := db.Query("SELECT Username, Email FROM User WHERE UserID > 0")
+	if err1 != nil {
+		fmt.Println("ratio, ", err1)
+		return
+	}
+	var Username string
+	var Email string
+	for result.Next() {
+		result.Scan(&Username, &Email)
+		fmt.Println(Username, Email)
+	}
+	result.Close()
+	db.Close()
+}
