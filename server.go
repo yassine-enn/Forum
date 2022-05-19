b8085ce9fc073f5cc3014f81c6b8e0917690d266
Yassine Ennouhi <root@LAPTOP-8BN54FDS.localdomain>
2022-05-19 14:05:35 +0200

    signup test working

--- server.go
+++ server.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -13,17 +16,39 @@ func main() {
 		return
 	}
 	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-
-		r.ParseForm()
-		r.FormValue("signupUsername")
-		r.FormValue("signupEmail")
-		r.FormValue("signupPassword")
-		r.FormValue("signupPasswordConfirm")
-		fmt.Println("username:", r.FormValue("username"))
-		fmt.Println("email:", r.FormValue("email"))
-		fmt.Println("password:", r.FormValue("password"))
-		fmt.Println("password_confirmation:", r.FormValue("password_confirmation"))
-
+		var UserID int
+		var userName string
+		var userEmail string
+		var userPassword string
+		var userAvatar string
+		if r.Method == "POST" {
+			UserID = 5
+			userName = r.FormValue("signupUsername")
+			userEmail = r.FormValue("signupEmail")
+			userPassword = r.FormValue("signupPassword")
+			userAvatar = "sasuke"
+			db, err := sql.Open("sqlite3", "./Forum.db")
+			if err != nil {
+				fmt.Println("Echec de l'ouverture de la base", err)
+				return
+			}
+			statement, prepareErr := db.Prepare("INSERT INTO User (UserID, Username, Email, PasswordHash, Avatar) VALUES (?,?,?,?,?)")
+			if prepareErr != nil {
+				fmt.Println("La préparation de la requête a échoué", prepareErr)
+				return
+			}
+			_, queryErr := statement.Exec(UserID, userName, userEmail, userPassword, userAvatar)
+			if queryErr != nil {
+				fmt.Println("Une erreur est survenue durant la requête", queryErr)
+				return
+			}
+			statement.Close()
+			db.Close()
+			fmt.Println("username:", r.FormValue("signupUsername"))
+			fmt.Println("email:", r.FormValue("signupEmail"))
+			fmt.Println("password:", r.FormValue("signupPassword"))
+			fmt.Println("password_confirmation:", r.FormValue("signupPasswordConfirm"))
+		}
 		tmpl.ExecuteTemplate(w, "acceuil", nil)
 	})
 	fileServer := http.FileServer(http.Dir("./template/"))
