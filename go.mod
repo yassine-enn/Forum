7ef7d71df0e7f984aa5eca5b9bef9a71845076de
Yassine Ennouhi <root@LAPTOP-8BN54FDS.localdomain>
2022-05-24 11:45:48 +0200

    sessions management

--- go.mod
+++ go.mod
@@ -6,3 +6,9 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.13
 	golang.org/x/crypto v0.0.0-20220518034528-6f7dac969898
 )
+
+require (
+	github.com/gorilla/context v1.1.1 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+)
--- server.go
+++ server.go
@@ -6,11 +6,16 @@ import (
 	"net/http"
 
 	exe "ImportFunction/ImportFunction"
+
+	"github.com/gorilla/context"
+	"github.com/gorilla/sessions"
 )
 
 type Page struct {
 	IsLoged bool
 }
+
+var store = sessions.NewCookieStore([]byte("super-secret"))
 
 func main() {
 	var isLog bool
@@ -24,8 +29,12 @@ func main() {
 			if r.FormValue("signup_button") == "LOG IN" {
 				isLog = exe.Login(r.FormValue("loginUsername"), r.FormValue("loginPassword"))
 				if isLog {
-					cookie := exe.CookieGenerator(r.FormValue("loginUsername"))
-					http.SetCookie(w, &cookie)
+					// cookie := exe.CookieGenerator(r.FormValue("loginUsername"))
+					// http.SetCookie(w, &cookie)
+					session, _ := store.Get(r, "session")
+					session.Values["userID"] = r.FormValue("loginUsername")
+					session.Save(r, w)
+
 				}
 			} else {
 				fmt.Println("pessi fraude finito")
@@ -36,13 +45,22 @@ func main() {
 				isLog = false
 			}
 		}
-		cookie, _ := r.Cookie(r.FormValue("loginUsername"))
-		fmt.Println(w, cookie)
+		// cookie, _ := r.Cookie(r.FormValue("loginUsername"))
+		// user := http.Cookie{
+		// 	Name: "Username", Value: r.FormValue("loginUsername"),
+		// }
+		// http.SetCookie(w, &user)
+		// fmt.Println("user:", user)
+		// fmt.Println(w, cookie)
 		data := Page{isLog}
 		tmpl.ExecuteTemplate(w, "acceuil", data)
+	})
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		data := Page{isLog}
+		tmpl.ExecuteTemplate(w, "index", data)
 	})
 	fileServer := http.FileServer(http.Dir("./template/"))
 	http.Handle("/template/", http.StripPrefix("/template/", fileServer))
 	fmt.Println("Listening on port 8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", context.ClearHandler(http.DefaultServeMux))
 }
