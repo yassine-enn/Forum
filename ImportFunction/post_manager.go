f9abff22e7cbda7d8d637f9f74f75623770044e6
francoisloue <francois.loue@ynov.com>
2022-06-01 09:59:55 +0200

    problem database for the yass

--- ImportFunction/post_manager.go
+++ ImportFunction/post_manager.go
@@ -1,6 +1,7 @@
 package ImportFunction
 
 import (
+	"database/sql"
 	"fmt"
 	"time"
 )
@@ -15,22 +16,20 @@ type Post struct {
 }
 
 func PostDataReader(condition string) []Post {
+
 	var postTable []Post
-<<<<<<< HEAD
 	db, err := sql.Open("sqlite3", "./forumdb")
 	if err != nil {
 		fmt.Println("Echec de l'ouverture de la base")
 		return nil
 	}
+	defer db.Close()
 	result, err1 := db.Query(`SELECT PostID, Date, PostText, PostTitle, likeCounter FROM Post WHERE PostID > 0`)
-=======
-	db := BddOpener()
-	result, err1 := db.Query(`SELECT PostID, Date, PostText, PostTitle, likeCounter, PostCategory FROM Post WHERE ` + condition)
->>>>>>> 35b068193f0429612d9fe07d79731cbd1afbd2e3
 	if err1 != nil {
 		fmt.Println("ratio, ", err1)
 		return nil
 	}
+	defer result.Close()
 	var PostID int
 	var PostTitle string
 	var PostText string
@@ -47,24 +46,14 @@ func PostDataReader(condition string) []Post {
 	return postTable
 }
 
-<<<<<<< HEAD
-func PostTopic(postText string, postTitle string, postCategory string, postImage string) {
-	db, err := sql.Open("sqlite3", "./forumdb")
-	if err != nil {
-		fmt.Println("Echec de l'ouverture de la base", err)
-		return
-	}
-=======
 func PostTopic(postText string, postTitle string, postCategory string) {
 	db := BddOpener()
->>>>>>> 35b068193f0429612d9fe07d79731cbd1afbd2e3
 	statement, prepareErr := db.Prepare("INSERT INTO Post (Date, PostCategory, PostText, Image, PostTitle, likeCounter) VALUES (?,?,?,?,?,?)")
 	if prepareErr != nil {
 		fmt.Println("La préparation de la requête a échoué", prepareErr)
 		return
 	}
-	date := string(time.Now().Format("02-01-2006"))
-	_, queryErr := statement.Exec(date, "", postText, "", postTitle, 0)
+	_, queryErr := statement.Exec(time.Now().Format("02-01-2006"), postCategory, postText, "", postTitle, 0)
 	if queryErr != nil {
 		fmt.Println("Une erreur est survenue durant la requête", queryErr)
 		return
